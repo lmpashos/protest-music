dd2772bcfc4f1b276f5c86975fe70e8414589ded
lmpashos <lmpashos@Jesus.local>
2014-11-19 20:35:25 -0500

    updated website
    we have an actual home page now

--- WEBSITE/index.php
+++ WEBSITE/index.php
@@ -14,6 +14,7 @@ $current_page = 'index';
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>Protest Music</title>
     <link href="css/style.css" rel="stylesheet" type="text/css" />
+    <link href="css/index.css" rel="stylesheet" type="text/css" />
     <script src="js/main.js"></script>
     <script src="js/index.js"></script>
 </head>
@@ -21,20 +22,22 @@ $current_page = 'index';
 <body>
     <div class="wrapper">
         <?php echo CommonHTML::get_navigation_menu($current_page); ?>  
-        <h1>Protest Music</h1>
-        <div class="content">
-            <div id="left">
-            	<p>This is the left content space</p>
-                <p>We can put stuff here</p>
-                <p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p>The spaces automatically resize based on the longest one</p>
-            </div>
-            <div id="right">
-            	<p>This is the right content space</p>
-                <p>We can put stuff here</p>
-            </div>
+        <h1>Home</h1>
+        <div class="content">         
             <div id="center">
-            	<p>This is the main content space</p>
-                <p>We can put stuff here</p>
+                <span id="splash_title">Discovering Protest Music:</span>
+                <span id="splash_subtitle">Can We Define It?
+                <p>
+                	A Digital Humanities Project by Leonidas Pashos, Katie Uihlein, and David Galloway
+                </p>
+                <div id="images">
+                    <img src="images/guthrie.jpg" />
+                    <img src="images/dylan.jpg" />
+                    <img src="images/nwa.jpg" />
+                </div>
+                <div id="button">
+              		<a href="about.php">Continue to About</a>
+                </div>
             </div>
         </div>     
     </div>
