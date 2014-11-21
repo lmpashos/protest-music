f64e6a674a21ee74c0a18b30649d29a028439e2a
David Galloway <gallowdd@hotmail.com>
2014-11-21 13:43:26 -0500

    Try to make conclusions.php validate

--- WEBSITE/conclusions.php
+++ WEBSITE/conclusions.php
@@ -9,13 +9,13 @@ $current_page = 'conclusions';
 
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html> 
+<html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title><?php echo ucfirst($current_page); ?></title>
     <link href="css/style.css" rel="stylesheet" type="text/css" />
-    <script src="js/main.js"></script>
+    <script src="js/main.js" type="text/javascript">/**/</script>
 </head>
 <body>
     <div class="wrapper">
