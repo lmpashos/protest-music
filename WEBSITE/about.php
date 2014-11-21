2b66bf979411cd1b75b3e9036d580495e0a9b92b
David Galloway <gallowdd@hotmail.com>
2014-11-21 09:25:59 -0500

    Allow for validation of about.php
    Passed validation for about.php

--- WEBSITE/about.php
+++ WEBSITE/about.php
@@ -8,13 +8,14 @@ error_reporting(-1);
 $current_page = 'about';
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title><?php echo ucfirst($current_page); ?></title>
     <link href="css/style.css" rel="stylesheet" type="text/css" />
-    <script src="js/main.js"></script>
+    <link href="css/about.css" rel="stylesheet" type="text/css" />
+    <script src="js/main.js" type="text/javascript"></script>
 </head>
 
 <body>
@@ -38,13 +39,13 @@ $current_page = 'about';
             	<p>Our research project focused on identify distinguishable linguistic features of protest music 
             	   and its lyrics. We wished to answer the following question: is there a way to identify protest 
             	   music by way of its lyrics?</p>
-            	<p>We approached this question by analyzing three time periods in America: 
+            	<p>We approached this question by analyzing three time periods in America:</p>
             	   <ul>
             	       <li>The 1930’s (during the Great Depression),</li>
             	       <li>The 1950’s and 60’s (during the Vietnam War),</li>
             	       <li>and the contemporary era, which we defined as post-1975.</li>
             	   </ul>
-            	   We compared lyrics to non-protest lyrics from the same time periods. We tagged the lyrics syntactically, 
+            	  <p>We compared lyrics to non-protest lyrics from the same time periods. We tagged the lyrics syntactically, 
             	   labeling noun phrases, verb phrases, and adverbs, also noting when a phrase is negated in certain instances. 
             	   We looked at the various phrases to see if there were distinctions between protest songs from one time period
             	   versus the non-protest songs from that same period. We used statistical analysis to determine if these 
